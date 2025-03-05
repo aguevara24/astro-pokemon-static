6aac227e04871ee73e4d5328975f526741887901
Alfredo Guevara Fuentes <aguevara24@hotmail.com>
2025-03-04 21:45:22 -0600

    Fin Sección 04

---
+++ src/components/pokemons/FavoritePokemonCard.tsx
@@ -0,0 +1,10 @@
+import type { FavoritePokemon } from "@interfaces/favorite-pokemon.ts";
+import type { Component } from "solid-js";
+
+interface Props {
+    pokemon: FavoritePokemon;
+}
+export const favoritePokemonCard: Component<Props> = (props) => {
+
+    return ();
+}
